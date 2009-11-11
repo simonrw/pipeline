24f57fec63a3b7345621017f37b1633efc993380
Simon Walker <s.r.walker101@googlemail.com>
2009-11-11 11:05:55 +0000

    extract returns a list of tables in the database

--- sql/extract.py
+++ sql/extract.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import numpy as np
 import os
 from optparse import OptionParser
 from subprocess import Popen, call, PIPE, STDOUT
@@ -13,15 +14,25 @@ def main():
     conn = sqlite3.connect('./data.db')
     c = conn.cursor()
 
-    c.execute('select * from testtable')
-    xcoords = []
-    ycoords = []
-    for row in c:
-       xcoords.append(row[1])
-       ycoords.append(row[2])
+    c.execute('select name from sqlite_master where type="table"')
+    r = c.fetchall() 
+    for entry in r:
+        print entry[0]
 
-    plt.scatter(xcoords, ycoords)
-    plt.show()
+
+
+    #xcoords = []
+    #ycoords = []
+    #for row in c:
+    #   xcoords.append(row[1])
+    #   ycoords.append(row[2])
+
+    #t = np.arange(len(xcoords)) 
+    #plt.subplot(211)
+    #plt.plot(t, xcoords, 'rx')
+    #plt.subplot(212)
+    #plt.plot(t, ycoords, 'rx')
+    #plt.show()
 
 if __name__ == '__main__':
     main()
