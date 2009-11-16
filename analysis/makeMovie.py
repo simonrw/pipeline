092f23c6551e8e68404d976b4ae39d78b4db0f17
Simon Walker <s.r.walker101@googlemail.com>
2009-11-16 17:03:33 +0000

    Got filelist

--- analysis/makeMovie.py
+++ analysis/makeMovie.py
@@ -1,15 +1,35 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.5
 
 import sys
 import os
 from optparse import OptionParser
 from subprocess import Popen, call, PIPE, STDOUT
+from astLib import astImages
+import pylab 
+import pyfits
 #from IPython.Shell import IPShellEmbed
 
 
-def main():
-    pass
+def main(arg):
+    dir = arg[1].rstrip('/')
+    p = Popen("ls %s" % dir, shell=True, stdout=PIPE)
+    filelist = p.communicate()[0].split()
+    filelist.remove('cmd')
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
-    main()
+
+    if len(sys.argv) != 2:
+        print >> sys.stderr, "Program usage: %s dir" % sys.argv[0]
+        exit(1)
+
+
+    main(sys.argv)
 
