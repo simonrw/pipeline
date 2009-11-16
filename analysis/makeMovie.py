48c63eb2703346767cc1c08b0c1266ab0ee5df00
Simon Walker <s.r.walker101@googlemail.com>
2009-11-16 18:17:47 +0000

    Movie creation possible but very specific

--- analysis/makeMovie.py
+++ analysis/makeMovie.py
@@ -5,8 +5,9 @@ import os
 from optparse import OptionParser
 from subprocess import Popen, call, PIPE, STDOUT
 from astLib import astImages
-import pylab 
+from jg.subs import progressbarClass
 import pyfits
+import time
 #from IPython.Shell import IPShellEmbed
 
 
@@ -14,15 +15,47 @@ def main(arg):
     dir = arg[1].rstrip('/')
     p = Popen("ls %s" % dir, shell=True, stdout=PIPE)
     filelist = p.communicate()[0].split()
-    filelist.remove('cmd')
+
+    pb = progressbarClass(len(filelist))
+
+    for i, file in enumerate(filelist):
+        if 'iKon' in file:
+            try:
+                hdulist = pyfits.open(dir + '/' + file)
+            except:
+                print >> sys.stderr, "Error opening file %s" % (dir + '/' + file)
+                exit(1)
+
+            header = hdulist[0].header
+            imdata = hdulist[0].data
+
+            hdulist.close()
+
+            size = int(header['naxis1']) * int(header['naxis2'])
+            cutLevels = ["smart", 99.5]
+
+            stub = file.split('.')[0]
+            outputFileName = '/tmp/core2/' + stub + '.png'
+            colorMapName = 'hot'
+
+            astImages.saveBitmap(outputFileName, imdata, cutLevels, size, colorMapName)
+            pb.progress(i) 
 
 
+    command = ('mencoder',
+               'mf:///tmp/*.png',
+               '-mf',
+               'type=png:w=800:h=600:fps=25',
+               '-ovc',
+               'lavc',
+               '-lavcopts',
+               'vcodec=mpeg4',
+               '-oac',
+               'copy',
+               '-o',
+               'output.avi')
 
-
-
-
-
-
+    os.spawnvp(os.P_WAIT, 'mencoder', command)
 
 if __name__ == '__main__':
 
@@ -30,6 +63,7 @@ if __name__ == '__main__':
         print >> sys.stderr, "Program usage: %s dir" % sys.argv[0]
         exit(1)
 
-
+    tstart = time.time()
     main(sys.argv)
+    print 'Time taken: %fs' % (time.time() - tstart)
 
