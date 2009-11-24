b9b4319b2ac4b19498f53c3945d804d39283fcd7
Simon Walker <s.r.walker101@googlemail.com>
2009-11-24 17:35:07 +0000

    Included fitsDir class

--- photometry/run.py
+++ photometry/run.py
@@ -24,7 +24,7 @@ from subprocess import Popen, PIPE, STDOUT, call
 from sys import argv, stderr, exit
 import os
 from optparse import OptionParser
-from modules import progressbarClass, _mkdir
+from modules import progressbarClass, fitsDir, _mkdir
 from time import time
 
 def printoutput(txt):
