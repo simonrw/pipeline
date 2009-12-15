967daa7848e4c045f511cb733939d153b17d0c37
Simon Walker <s.r.walker101@googlemail.com>
2009-12-15 17:36:29 +0000

    finalised error message for plot program

--- analysis/plotSingleLightcurve.sh
+++ analysis/plotSingleLightcurve.sh
@@ -2,5 +2,5 @@
 
 file=`mktemp`
 
-./extractSingle.sh $@ > $file
-echo "set term wx; unset key; plot '${file}' u 1" | gnuplot -persist
+/home/astro/phrfbf/work/phd/pipeline/analysis/extractSingle.sh $@ > $file
+echo "set term wx; unset key; plot '${file}' u 1" | gnuplot -persist 2> /dev/null
